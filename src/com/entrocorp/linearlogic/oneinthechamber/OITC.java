96db109b8f43ec0db7b518dfe87de79735e5a407
Jared Heath <silkrawnstudios@gmail.com>
2013-08-05 08:49:39 -1000

    Add in-game prefix with option to abbreviate

--- src/com/entrocorp/linearlogic/oneinthechamber/OITC.java
+++ src/com/entrocorp/linearlogic/oneinthechamber/OITC.java
@@ -1,15 +1,20 @@
 package com.entrocorp.linearlogic.oneinthechamber;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OITC extends JavaPlugin {
 
     public static OITC instance;
+    public static String prefix;
 
     public void onEnable() {
         instance = this;
         logInfo("Loading the config...");
         saveDefaultConfig();
+        prefix = ChatColor.GRAY + "[" + ChatColor.GOLD + (getConfig().getBoolean("use-abbreviated-prefix") ? "OITC" : "O" +
+                ChatColor.DARK_GRAY + "ne" + ChatColor.GOLD + "I" + ChatColor.DARK_GRAY + "n" + ChatColor.GOLD + "T" +
+                ChatColor.DARK_GRAY + "he" + ChatColor.GOLD + "C" + ChatColor.DARK_GRAY + "hamber") + ChatColor.GRAY + "] ";
         logInfo("Successfully enabled. Game on!");
     }
 
