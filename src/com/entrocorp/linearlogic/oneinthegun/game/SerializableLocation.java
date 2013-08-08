60e1f7f0204c252e980e93d98c825769ab6e4e31
Jared Heath <silkrawnstudios@gmail.com>
2013-08-08 03:49:38 -1000

    Add yaw and pitch to SerializableLocation; don't convert to Locations in equals(…)

--- src/com/entrocorp/linearlogic/oneinthegun/game/SerializableLocation.java
+++ src/com/entrocorp/linearlogic/oneinthegun/game/SerializableLocation.java
@@ -17,6 +17,8 @@ public class SerializableLocation implements Serializable {
     private double x;
     private double y;
     private double z;
+    private float yaw;
+    private float pitch;
     private String worldName;
 
     public SerializableLocation(Location loc) {
@@ -26,17 +28,22 @@ public class SerializableLocation implements Serializable {
         x = loc.getX();
         y = loc.getY();
         z = loc.getZ();
+        yaw = loc.getYaw();
+        pitch = loc.getPitch();
     }
 
     public Location asBukkitLocation() {
         World world = OITG.instance.getServer().getWorld(worldName);
-        return world == null ? null : new Location(world, x, y, z);
+        return world == null ? null : new Location(world, x, y, z, yaw, pitch);
     }
 
     public boolean equals(Object loc) {
         if (!(loc instanceof SerializableLocation))
             return false;
         SerializableLocation compare = (SerializableLocation) loc;
-        return compare.asBukkitLocation().equals(asBukkitLocation()) ? true : false;
+        if (x == compare.x && y == compare.y && z == compare.z && yaw == compare.yaw && pitch == compare.pitch &&
+                worldName.equalsIgnoreCase(compare.worldName))
+            return true;
+        return false;
     }
 }
