2eb43cbf87320e94d76d57fd4120385e8152c3df
Jared Heath <silkrawnstudios@gmail.com>
2013-08-11 10:46:53 -0400

    Add optional "state" parameter to CommandArenas

--- src/com/entrocorp/linearlogic/oneinthegun/commands/CommandArenas.java
+++ src/com/entrocorp/linearlogic/oneinthegun/commands/CommandArenas.java
@@ -10,15 +10,20 @@ import com.entrocorp.linearlogic.oneinthegun.OITG;
 public class CommandArenas extends OITGCommand {
 
     public CommandArenas(CommandSender sender, String[] args) {
-        super(sender, args, 0, "arenas", "oneinthegun.arena.list", false);
+        super(sender, args, 0, "arenas [state]", "oneinthegun.arena.list", false);
     }
 
     public void run() {
+        String waiting = ChatColor.GREEN + "\nWaiting: " + ChatColor.GRAY + Arrays.toString(OITG.instance.getArenaManager().getWaitingArenas()),
+                starting = ChatColor.YELLOW + "\nStarting: " + ChatColor.GRAY + Arrays.toString(OITG.instance.getArenaManager().getStartingArenas()),
+                ingame = ChatColor.RED + "\nIn game: " + ChatColor.GRAY + Arrays.toString(OITG.instance.getArenaManager().getIngameArenas()),
+                closed = ChatColor.DARK_RED + "\nClosed: " + ChatColor.GRAY + Arrays.toString(OITG.instance.getArenaManager().getClosedArenas());
+
         sender.sendMessage(ChatColor.GRAY + "[]" + ChatColor.RED + "===" + ChatColor.GRAY + "[" + ChatColor.DARK_RED +
-                "Arenas" + ChatColor.GRAY + "]" + ChatColor.RED + "===" + ChatColor.GRAY + "[]\n" + ChatColor.GREEN +
-                "Waiting: " + ChatColor.GRAY + Arrays.toString(OITG.instance.getArenaManager().getWaitingArenas()) +
-                ChatColor.RED + "\nIn game: " + ChatColor.GRAY + Arrays.toString(OITG.instance.getArenaManager().getIngameArenas()) +
-                ChatColor.DARK_RED + "\nClosed: " + ChatColor.GRAY + Arrays.toString(OITG.instance.getArenaManager().getClosedArenas()));
+                "Arenas" + ChatColor.GRAY + "]" + ChatColor.RED + "===" + ChatColor.GRAY + "[]" + (args.length == 0 ?
+                waiting + starting + ingame + closed : args[0].equalsIgnoreCase("waiting") ? waiting :
+                args[0].equalsIgnoreCase("starting") ? starting : args[0].equalsIgnoreCase("ingame") ? ingame :
+                args[0].equalsIgnoreCase("closed") ? closed : waiting + starting + ingame + closed));
     }
 
 }
