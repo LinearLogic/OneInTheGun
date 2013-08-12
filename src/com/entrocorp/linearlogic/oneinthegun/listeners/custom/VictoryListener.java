8b54c16c3aecf1b41340a9dc549df7927ba6cc3e
Jared Heath <silkrawnstudios@gmail.com>
2013-08-12 01:33:31 -0400

    [API] Add VictoryListener interface to enable the rewarding of arena winners

---
+++ src/com/entrocorp/linearlogic/oneinthegun/listeners/custom/VictoryListener.java
@@ -0,0 +1,8 @@
+package com.entrocorp.linearlogic.oneinthegun.listeners.custom;
+
+import org.bukkit.entity.Player;
+
+public interface VictoryListener {
+
+    public void onVictory(Player victor);
+}
